6d82c93f54dea4955b0bad1497e650488612046c
SuperSkillz96 <102481906+SuperSkillz96@users.noreply.github.com>
2023-10-24 11:24:03 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCdrkMe6wrnJOR-oG8yrHb5NvPFa266dMk",
+    authDomain: "spectram-app-52f25.firebaseapp.com",
+    projectId: "spectram-app-52f25",
+    storageBucket: "spectram-app-52f25.appspot.com",
+    messagingSenderId: "82606404125",
+    appId: "1:82606404125:web:226bb85e87ed3f73560cf4"
+};
